346ab4cd6cc109045fcc18fb8277b82dedef13ee
Gentrit <gentrithoti@hotmail.com>
2023-05-25 21:48:40 +0200

    hello

--- Server/Services/CartItemService.cs
+++ Server/Services/CartItemService.cs
@@ -17,38 +17,30 @@ public class CartItemService : ICartItemService
     public async Task<CartItem> AddCartItemAsync(int productId)
     {
         var cartItem = new CartItem();
-        // Fetch the product from the database
         var product = await _context.Products.FindAsync(productId);
 
-        // Check if the product exists
         if (product == null)
             throw new ArgumentException("Invalid product ID");
 
-        // Check if a CartItem with this product already exists
         var existingCartItem = await _context.CartItems.FirstOrDefaultAsync(ci => ci.ProductId == productId);
 
         if (existingCartItem != null)
         {
-            // If the CartItem already exists, increase the quantity
             existingCartItem.Quantity += 1;
         }
         else
         {
-            // If the CartItem does not exist, create a new one
             cartItem = new CartItem
             {
                 ProductId = product.Id,
-                Quantity = 1  // Set this to the desired initial quantity
-                              // Set other CartItem properties as needed
+                Quantity = 1
             };
 
             await _context.CartItems.AddAsync(cartItem);
         }
 
-        // Save the changes to the database
         await _context.SaveChangesAsync();
 
-        // Return the updated or new CartItem
         return existingCartItem ?? cartItem;
     }
 
