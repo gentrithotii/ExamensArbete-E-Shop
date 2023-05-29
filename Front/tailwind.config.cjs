6897b95e50c57d19a961966c73ed8024efc3460c
Gentrithotii <Gentrithoti@hotmail.com>
2023-05-29 18:12:57 +0200

    Tailwind errors

--- Front/tailwind.config.cjs
+++ Front/tailwind.config.cjs
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       gridTemplateRows: {
